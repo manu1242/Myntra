61493bae5849d5b42fe57102b39880ef163b4feb
ramgopi <ramgopiramgopi231@gmail.com>
2025-09-23 16:19:25 +0530

    Updated the Navigation Page

--- src/Components/NavigationPage/Genz.jsx
+++ src/Components/NavigationPage/Genz.jsx
@@ -1,91 +1,159 @@
 import "./Genz.css";
-function Genz(){
-    return (<>
-       <div className="Genzcont">
-          <div className="innercontainer">
-            <h5 className="gh5">Indian& Fusion wear</h5>
-            <li>Kurtas & Suits</li>
-            <li>Kurtas,Tunics</li>
-            <li>FormalShirts</li>
-            <li>SweatShirts</li>
-            <li>Sweaters</li>
-            <li>Jackets</li>
-            <li>Blazers&Coats</li>
-            <li>Suits</li>
-            <li>Rain Jackets</li>
-            <hr></hr>
-            <h5 className="gh5">Indian & Festive Wear </h5>
-            <li>Kurtas Sets</li>
-            <li>Sherwanis</li>
-            <li>Nehru Jackets</li>
-            <li>Dhoits</li>
-          </div>
+function Genz() {
+  const list = [
+    "Kurtas & Suits",
+    "Kurtas,Tunics",
+    "FormalShirts",
+    "SweatShirts",
+    "Sweaters",
+    "Jackets",
+    "Blazers&Coats",
+    "Suits",
+    "Rain Jackets",
+  ];
 
-          <div className="innercontainer2">
-            <h5 className="gh5">Bottom Wear</h5>
-            <li>Jeans</li>
-            <li>Casual Trousers</li>
-            <li>Formal Trousers</li>
-            <li>Shorts</li>
-            <li>TrackPants&Joggers</li>
-            <hr></hr>
-            <h5 className="gh5">Inner Wear & sleepwear</h5>
-            <li>Briefs&Trunks</li>
-            <li>Boxers</li>
-            <li>Vests</li>
-            <li>Sleepwear</li>
-            <li>Thermals</li>
-          </div>
+  const list2 = ["Kurtas Sets", "Sherwanis", "Nehru Jackets", "Dhoits"];
 
-          <div className="innercontainer3">
-            <h5 className="gh5">Footwear</h5>
-            <li>Casual shoes</li>
-            <li>Sports shoes</li>
-            <li>Formalshoes</li>
-            <li>Sneakers</li>
-            <li>Sandels </li>
-            <li>Flip Flops</li>
-            <li>Socks</li>
-            <hr></hr>
-          </div>
+  const list3 = [
+    "Jeans",
+    "Casual Trousers",
+    "Formal Trousers",
+    "Shorts",
+    "TrackPants & Joggers",
+  ];
 
-          <div className="innercontainer4">
-            <h5 className="gh5">Sports & active Wear</h5>
-            <li>Sports Shoes</li>
-            <li>Sports Sandles</li>
-            <li>Active T-Shirts</li>
-            <li>TrackPants&shorts</li>
-            <li>Track Suits</li>
-            <li>Jackets&sweatersShirts</li>
-            <li>Sports Accessories</li>
-            <li>Swimwear</li>
-            <hr></hr>
-            <h5 className="gh5">Gadgets</h5>
-            <li>Smart Wearables</li>
-            <li>Fitness Gadgets</li>
-            <li>HeadPhone</li>
-            <li>Speakers</li>
-          </div>
+  const list4 = ["Briefs&Trunks", "Boxers", "Vests", "Sleepwear", "Thermals"];
 
-          <div className="innercontainer5">
-            <h5 className="gh5">Fashion Accessories</h5>
-            <li>Wallets</li>
-            <li>Belts</li>
-            <li>Perfumes & Body Mists</li>
-            <li>Trimmers</li>
-            <li>Deodorants</li>
-            <li>Ties,Cufflinks & pockets Squares</li>
-            <li>Accessory Gifts Sets</li>
-            <li>Caps & Hats</li>
-            <li>PhoneCases</li>
-            <li>Rings & WirstWear</li>
-            <li>Helmets</li>
-            <hr></hr>
-            <h5 className="gh5">Bags & Backpacks</h5>
-            <h5 className="gh5">Luggages & Trolleys</h5>
-          </div>
+  const list5 = [
+    "Casual shoes",
+    "Sports shoes",
+    "Formalshoes",
+    "Sneakers",
+    "Sandels",
+    "Flip Flops",
+    "Socks",
+  ];
+
+  const list6 = [
+    "Sports Shoes",
+    "Sports Sandles",
+    "Active T-Shirts",
+    "TrackPants&shorts",
+    "Track Suits",
+    "Jackets&sweatersShirts",
+    "Sports Accessories",
+    "Swimwear",
+  ];
+
+  const list7 = ["Smart Wearables", "Fitness Gadgets", "HeadPhone", "Speakers"];
+
+  const list8 = [
+    "Wallets",
+    "Belts",
+    "Perfumes & Body Mists",
+    "Trimmers",
+    "Deodorants",
+    "Ties,Cufflinks & pockets Squares",
+    "Accessory Gifts Sets",
+    "Caps & Hats",
+    "PhoneCases",
+    "Rings & WirstWear",
+    "Helmets",
+  ];
+
+  return (
+    <>
+      <div className="Beautycont">
+        <div className="innercontainer">
+          <h5 className="gh5">Indian& Fusion wear</h5>
+          {list.map((value) => {
+            return (
+              <>
+                <li>{value}</li>
+              </>
+            );
+          })}
+          <hr></hr>
+          <h5 className="gh5">Indian & Festive Wear </h5>
+          {
+          list2.map((value) => {
+            return (
+              <>
+                <li>{value}</li>
+              </>
+            );
+          })
+          }
         </div>
-    </>);
+
+        <div className="innercontainer2">
+          <h5 className="gh5">Bottom Wear</h5>
+          {list3.map((value) => {
+            return (
+              <>
+                <li>{value}</li>
+              </>
+            );
+          })}
+          <hr></hr>
+          <h5 className="gh5">Inner Wear & sleepwear</h5>
+          {list4.map((value) => {
+            return (
+              <>
+                <li>{value}</li>
+              </>
+            );
+          })}
+        </div>
+
+        <div className="innercontainer3">
+          <h5 className="gh5">Footwear</h5>
+          {list5.map((value) => {
+            return (
+              <>
+                <li>{value}</li>
+              </>
+            );
+          })}
+          <hr></hr>
+        </div>
+
+        <div className="innercontainer4">
+          <h5 className="gh5">Sports & active Wear</h5>
+          {list6.map((value) => {
+            return (
+              <>
+                <li>{value}</li>
+              </>
+            );
+          })}
+          <hr></hr>
+          <h5 className="gh5">Gadgets</h5>
+          {list7.map((value) => {
+            return (
+              <>
+                <li>{value}</li>
+              </>
+            );
+          })}
+        </div>
+
+        <div className="innercontainer5">
+          <h5 className="gh5">Fashion Accessories</h5>
+          {list8.map((value) => {
+            return (
+              <>
+                <li>{value}</li>
+              </>
+            );
+          })}
+          <hr></hr>
+          <h5 className="gh5">Bags & Backpacks</h5>
+          <h5 className="gh5">Luggages & Trolleys</h5>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Genz;
